0fb87dfb46060ce6dbd51c9baa9d5e115dec5f1e
552020 <stefanolombardo@posteo.de>
2023-12-03 15:12:59 +0100

    one gold star

--- Day02/part_one.py
+++ Day02/part_one.py
@@ -1,31 +1,47 @@
 lines_dict = {}
 
-with open('input_files/input_one_test.txt', 'r') as file:
+with open('input_files/input_one.txt', 'r') as file:
 	for idx, line in enumerate(file, start=1):
-		print(idx, line)
+		# print(idx, line)
 		parts = line.split(':', 1)
-		if len(parts) > 1:
-			lines_dict[idx] = parts[1].strip()
+		game_nbr = None
+		for el in parts[0].split():
+			if el.isdigit():
+				game_nbr = int(el)
+				break
+		if game_nbr is not None and len(parts) > 1:
+			cleaned_str = parts[1].replace(',', '').replace(';', '').strip()
+			lines_dict[game_nbr] = cleaned_str
 		else:
 			lines_dict[idx] = ''
 
 total = 0
 
-for line_nbr, string in lines_dict.items():
-	print(line_nbr, string)
+for game_id, string in lines_dict.items():
+	print("game id:", game_id)
+	print("string:", string)
+	# print(game_id, string)
 	# string.split() return a list
 	parts = string.split()
+	flag = 0
 	for idx, part in enumerate(parts):
-		if part.isdigit() and int(part) > 15:
-			break
+		# print(idx)
+		# print(part)
 		if part.isdigit() and int(part) > 14:
-			# trying to access an idx outside the range woudl throw an IndexError!
-			if idx + 1 < len(parts) and parts[idx + 1] != 'red':
-				break
+			flag = 1
+			break
 		if part.isdigit() and int(part) > 13:
-			if idx + 1 < len(parts) and parts[idx + 1] != 'red' and parts[idx + 1] != 'green':
+			# trying to access an idx outside the range woudl throw an IndexError!
+			if (parts[idx + 1] != 'blue' and parts[idx + 1] != 'red'):
+				flag = 1
 				break
-	else:
-		total += line_nbr
+		if part.isdigit() and int(part) > 12:
+			# print("break > 12 and red")
+			if parts[idx + 1] == 'red':
+				flag = 1
+				break
+	if flag == 0:
+		print(game_id, string)
+		total += game_id
 
 print(total)
